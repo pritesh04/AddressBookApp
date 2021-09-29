e05045c99a1d1c5d4db4e791def53fa8b86c2f09
MSDOMAIN1\pbhanga <priteshraj.bhangale@morningstar.com>
2021-09-29 15:48:52 +0530

    [Pritesh] Created Http methods For AddressBook

--- src/main/java/com/AddressBookApp/controller/AddressBookController.java
+++ src/main/java/com/AddressBookApp/controller/AddressBookController.java
@@ -5,7 +5,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.RequestEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.AddressBookApp.model.AddressBook;
@@ -13,7 +18,26 @@ import com.AddressBookApp.service.AddressBookService;
 
 @RestController
 public class AddressBookController {
+
+	@GetMapping("/get")
+	public String getAdd() {
+		return "Getting contacts";
+	}
 	
+	@GetMapping("/getId")
+	public String getAddById(@RequestParam(value = "id")int id) {
+		return "displaying specific  id contact";
+	}
+	
+	@PutMapping("/put/{id}")
+	public String putAdd(@PathVariable int id) {
+		return "contact updated";
+	}
+	
+	@DeleteMapping("/delete/{id}")
+	public String deleteAdd(@PathVariable int id) {
+		return "Contact Deleted";
+	}
 	
 	
 
